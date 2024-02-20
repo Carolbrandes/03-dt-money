88d74c3f94dcf49de3b6d6824ba13b6d690f1c64
carol <carolina.brandes@corebiz.ag>
2024-02-20 14:29:48 -0300

    class add buttons in new transaction form income and outcome

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,5 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { X } from "phosphor-react";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
 import * as S from "./styles";
 
 export const NewTransactionModal = () => {
@@ -19,6 +19,18 @@ export const NewTransactionModal = () => {
             <input type="text" placeholder="Descrição" required />
             <input type="number" placeholder="Preço" required />
             <input type="text" placeholder="Categoria" required />
+
+            <S.TransactionType>
+              <S.TransactionTypeButton variant="income" value="income">
+                <ArrowCircleUp size={24} />
+                Entrada
+              </S.TransactionTypeButton>
+
+              <S.TransactionTypeButton variant="outcome" value="outcome">
+                <ArrowCircleDown size={24} />
+                Saída
+              </S.TransactionTypeButton>
+            </S.TransactionType>
 
             <button type="submit">Cadastrar</button>
           </form>
